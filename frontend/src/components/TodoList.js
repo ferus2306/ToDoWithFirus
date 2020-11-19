47ca8f077de43d8ae35b7cf468a3b67d56dfba67
Firus Hanov <d00363593@dmail.dixie.edu>
2020-11-18 22:30:18 -0700

    readme description, filter feature start

---
+++ frontend/src/components/TodoList.js
@@ -0,0 +1,104 @@
+import React, { useState } from 'react'
+import TodoForm from './TodoForm.js'
+import Todo from './Todo.js'
+import { Container, Col, Row, Form } from 'react-bootstrap'
+
+
+function TodoList() {
+
+    const [todos, setTodos] = useState([
+        {id: 8, task: "Read Peace and War by Leo Tolstoy", completed: false },
+        {id: 9, task: "Participate in CodeCamp", completed: true },
+        {id: 10, task: "Prepare dinner", completed: false }
+    ]);
+
+    const [checkedPendingFilter, setCheckedPendingFilter] = useState();
+
+
+    
+
+
+    const updateTodo = (todoId, newValue) => {
+        console.log('update to do function')
+        if (!newValue.task || /^\s*$/.test(newValue.task)) {
+            return;
+        }
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item))
+        );
+    }
+
+
+
+    const addTodo = todo => {
+        console.log('Add todo')
+        if (!todo.task || /^\s*$/.test(todo.task)) {
+            return;
+        }
+        const newTodos = [todo, ...todos];
+        setTodos(newTodos);
+        console.log(...todos);
+    }
+
+    const deleteTodo = (todoIndex) => {
+        const newTodos = todos.filter((_, index) => index !== todoIndex);
+        setTodos(newTodos);
+    }
+
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    };
+
+    const changeCheckedPending = id => {
+        console.log('Pending checked')
+        setCheckedPendingFilter(true);
+    }
+
+
+
+
+
+
+
+
+    return (
+        <Container>
+          <Row className="app d-flex justify-content-center">
+                <Col className="todo-list">
+                    <TodoForm className="" onSubmit={addTodo} />
+
+                    <div className="d-flex justify-content-center py-4">
+                        <Form.Check
+                            className="mx-2"
+                            type="checkbox" label="Filter by Pending" 
+                            onChange={changeCheckedPending}    
+                        />
+                        <Form.Check className="mx-2" type="checkbox" label="Filter by Completed" />
+                    </div>
+                    
+                    {todos.length < 1 &&
+                        <h3 className="text-center">You have no items in To Do List.</h3>
+                    }
+
+                    <Todo
+                        todos={todos}
+                        deleteTodo={deleteTodo}
+                        completeTodo={completeTodo}
+                        updateTodo={updateTodo}
+                        checkedPendingFilter={true}
+                        
+                    />
+                </Col>
+          </Row>
+        </Container>
+    )
+}
+
+export default TodoList;
+
